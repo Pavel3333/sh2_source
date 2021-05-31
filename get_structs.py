2e2def501b27494c435204d80f8003888f6d9003
Pavel3333 <pavel.ushaev2012@yandex.ru>
2021-05-31 23:44:23 +0300

    add structs parsing code

---
+++ get_structs.py
@@ -0,0 +1,134 @@
+import os
+import re
+
+def writeRaw(fil, data):
+    fil.write(data) 
+
+def writeNewlined(fil, data):
+    writeRaw(fil, data + '\n') 
+
+def writeIndented(fil, indent, data):
+    writeNewlined(fil, ('\t' * indent) + data)
+
+_STRUCT_FILE_NAME = 'offsets.c'
+
+_STRUCT_DEF_TEMPLATE = r'typedef struct ([\w\d_]+);$'
+_STRUCT_START_TEMPLATE = r'^struct ([\w\d_]+)$'
+_STRUCT_END_TEMPLATE = r'^\};$'
+_STRUCT_FIELD_PARSE_TEMPLATE = r'^\s+([\w\d *]+) ([\w\d]+)\s*([\[\d\]]+)?\s*(:\s*[\d]+)?;'
+_SUBSTRUCT_START_TEMPLATE_1 = r'^\s*struct\s*$'
+_SUBSTRUCT_START_TEMPLATE_2 = r'^\s*{\s*$'
+_SUBSTRUCT_END_TEMPLATE = r'^\s*};\s*$'
+
+_MAIN_FUNCTION_START = """
+#include <stdio.h>
+
+int main() {
+    printf("structsSizes = {\\n");
+"""
+
+_STRUCT_SIZE_PRINT_FMT = (' ' * 4) + r'printf("    \"{0}\": %d,\n", sizeof({0}));'
+
+_MAIN_FUNCTION_END = """
+    printf("}\\n");
+
+    return 0;
+}
+"""
+
+
+def isSubstructureContent(line):
+    return not any(
+        re.match(template, line)
+        for template in (
+            _SUBSTRUCT_START_TEMPLATE_1,
+            _SUBSTRUCT_START_TEMPLATE_2,
+            _SUBSTRUCT_END_TEMPLATE
+        )
+    )
+
+
+structsDefs = set()
+structsData = {}
+
+for root, directories, files in os.walk('./'):
+    if not root.endswith('/'):
+        root += '/'
+
+    for filename in files:
+        if not filename.endswith('.c') or filename == _STRUCT_FILE_NAME:
+            continue
+
+        path = (root + filename)[len('./'):]
+        
+        print 'Processing:', path
+
+        lastFoundStructName = None
+        lastFoundStructData = ''
+
+        with open(path, 'r') as source:
+            for line in source:
+                preparedLine = line.replace('_anon', '_%s_anon' % filename[:-2]).replace('<unknown fundamental type (0xa510)>', 'void')
+                
+                if lastFoundStructName is not None:
+                    lastFoundStructData += preparedLine
+                    
+                    endMatch = re.match(_STRUCT_END_TEMPLATE, preparedLine)
+                    if endMatch is not None:
+                        # print '\tStruct %s ended' % lastFoundStructName
+                        structsData[lastFoundStructName] = lastFoundStructData
+                        lastFoundStructName = None
+                else:
+                    defMatch = re.match(_STRUCT_DEF_TEMPLATE, preparedLine)
+                    if defMatch is not None:
+                        structsDefs.add(preparedLine)
+                        continue
+                    
+                    startMatch = re.match(_STRUCT_START_TEMPLATE, preparedLine)
+                    if startMatch is None:
+                        continue
+
+                    foundStructName = startMatch.group(1)
+                    if foundStructName in structsData:
+                        continue
+
+                    # print '\tStruct %s started' % foundStructName
+                    lastFoundStructName = foundStructName
+                    lastFoundStructData = preparedLine
+
+
+for structName, structData in structsData.iteritems():
+    for line in structData.split('\n')[1:-1]:
+        if not isSubstructureContent(line):  # TODO: union / enum processing
+            continue
+
+        match = re.match(_STRUCT_FIELD_PARSE_TEMPLATE, line)
+        if match:
+            pass
+            # print 'type: %s, name: %s, count: %s, bit count: %s' % (
+            #     match.group(1),
+            #     match.group(2),
+            #     match.group(3),
+            #     match.group(4)
+            # )
+        else:
+            print line
+
+with open(_STRUCT_FILE_NAME, 'wb') as structsFile:
+    for structDef in sorted(structsDefs):
+        writeRaw(structsFile, structDef)
+
+    writeRaw(structsFile, '\n\n')
+    
+    for structName, structData in structsData.iteritems():
+        print 'Struct %s writed' % structName
+        writeNewlined(structsFile, structData)
+
+    writeRaw(structsFile, _MAIN_FUNCTION_START)
+
+    for structName in structsData:
+        writeNewlined(structsFile, _STRUCT_SIZE_PRINT_FMT.format(structName))
+    
+    writeRaw(structsFile, _MAIN_FUNCTION_END)
+
+print '---DONE---'
